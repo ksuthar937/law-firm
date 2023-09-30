e6484cfcec038d664c0b53ace37238c311e4e905
the Viper Kailash Suthar <ksuthar937@gmail.com>
2023-09-30 20:16:21 +0530

    added footer section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./components/About/About";
 import Clients from "./components/Clients/Clients";
 import FAQ from "./components/FAQ/FAQ";
+import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import Newsletter from "./components/Newsletter/Newsletter";
 import Practices from "./components/Practices/Practices";
@@ -17,6 +18,7 @@ function App() {
       <Team />
       <FAQ />
       <Newsletter />
+      <Footer />
     </div>
   );
 }
